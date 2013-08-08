43996e760fc026c6cc0dbcab75f974bb366b2e67
David C Muller <davidmull@gmail.com>
2013-08-08 17:35:40 +0200

    working version of p01_sim_nomatch.r

--- analysis/p01_sim_nomatch.r
+++ analysis/p01_sim_nomatch.r
@@ -15,6 +15,7 @@
 library(survival)
 library(devtools)
 library(data.table)
+library(ggplot2)
 version_randsurv <- "0.1"
 version_ncctools <- "0.2"
 if ("randsurv" %in% rownames(installed.packages()) == FALSE) {
@@ -54,7 +55,7 @@ coef_se <- function(mod) {
   }
   res <- as.data.frame(rbind(c(mod$coefficients, log(mod$scale)),
                              c(sqrt(diag(mod$var)))))
-  colnames(res) <- c(names(mod$coefficients), "Log(scale)")
+  colnames(res) <- c("intercept", "exposure", "log_scale")
   res$param <- c("coef", "se")
   return(res)
 }
@@ -65,10 +66,10 @@ coef_se <- function(mod) {
 # function to display dots to show progress of, and provide
 # entertainment during, simulations
 dots <- function(iter) {
-  di_str <- ifelse((iter %% 10) == 0,
-                   paste0(iter, "\n"),
+  di_str <- ifelse(((iter %% 10) == 0) & !(iter %% 50 == 0),
+                   paste0(iter),
                    ifelse((iter %% 50) == 0,
-                          paste(iter),
+                          paste(iter, "\n"),
                           "."))
   return(di_str)
 }
@@ -91,9 +92,15 @@ sim_no_match <- function(params) {
   cohort$failed <- as.numeric(cohort$status==1)
   cohort$exposure <- exposure
   # full cohort analysis
-  full <- survreg(Surv(time = cohort$obs_time, event = cohort$failed) ~ exposure, 
+  full <- survreg(Surv(time = cohort$obs_time, event = cohort$failed) ~ cohort$exposure, 
                   dist = "weibull")
   full <- coef_se(full)
+  coxfull <- coxph(Surv(time = cohort$obs_time, event = cohort$failed) ~ cohort$exposure)
+  coxfull <- data.frame(intercept = rep(NA ,2), 
+                        exposure = c(coef(coxfull), sqrt(diag(vcov(coxfull)))),
+                        log_scale = rep(NA, 2),
+                        param = c("coef", "se"),
+                        stringsAsFactors=FALSE)
   
   # sample ncc
   ncc <- ncc_sample(entry = 0, 
@@ -103,24 +110,36 @@ sim_no_match <- function(params) {
                     include = list(exposure, obs_time),
                     data = cohort,
                     silent = TRUE)
-  ncc <- as.data.frame(ncc)
+  # some people might have been sampled as controls more than once, 
+  # keep only one record
+  setkey(ncc, ncc_id, ncc_fail)
+  ncc <- as.data.frame(ncc[, tail(.SD, 1), by = c("ncc_id")])
+    
   # generate survival object
   st <- Surv(time = ncc$obs_time, event = ncc$ncc_fail)
   weighted <- survreg(st ~ exposure,  
                       weights = 1/ncc_pr,
                       data = ncc) 
   weighted <- coef_se(weighted)
+  coxweighted <- coxph(st ~ exposure,  
+                      weights = 1/ncc_pr,
+                      data = ncc) 
+  coxweighted <- data.frame(intercept = rep(NA, 2),
+                            exposure = c(coef(coxweighted), sqrt(diag(vcov(coxweighted)))),
+                            log_scale = rep(NA, 2),
+                            param = c("coef", "se"),
+                            stringsAsFactors = FALSE)
   
   full$model <- "full"
   weighted$model<- "weighted"
-  res <- rbindlist(list(full, weighted))
-  iter <- ifelse(exists("iter", inherits=FALSE), iter+1, 1)
-  cat(dots(iter))
+  coxfull$model <- "coxfull"
+  coxweighted$model <- "coxweighted"
+  res <- rbindlist(list(full, weighted, coxfull, coxweighted))
   return(res)
 }
 
 # set up parameters that are constant accross scenarios
-cohort_size <- 10000
+cohort_size <- 1000
 cens <- 80
 baseline_rate_event <- 10e-10
 baseline_rate_compet <- 10e-9
@@ -143,16 +162,48 @@ sim_settings <- list(
 ##############################
 # Run simulation
 ##############################
-nsims <- 500
+nsims <- 200
 # seed chosen by sampling a random integer between 1 and 9999
 # at <www.random.org> 
 set.seed(3211)
 output <- vector(mode = "list", length(sim_settings))
 for(i in 1:length(sim_settings)) {
   cat(paste0("Simulating for parameter set ", i, ":\n"))
-  output[[i]] <- replicate(nsims, 
-                           sim_no_match(sim_settings[[i]]), 
-                           simplify=FALSE)
+  simres <- vector(mode = "list", nsims)
+  for (j in 1:nsims) {
+    simres[[j]] <- tryCatch(sim_no_match(sim_settings[[i]]),
+                       warning = function(e) {
+                         warning("there was a warning")
+                         return(NULL)
+                       },
+                       error = function(e) {
+                         warning("there was an error")
+                         return(NULL)
+                       })
+    cat(dots(j))
+  }
+  output[[i]] <- rbindlist(simres) 
 }
-out <- rbindlist(output[[1]])
+out <- rbindlist(output)
 write.csv(out, file="./analysis/output/o01_sim_nomatch.csv")
+
+cox <- out[(model=="coxfull" | model=="coxweighted") & param=="coef",]
+h <- ggplot(data=cox, aes(x=exposure))
+h + geom_histogram() + facet_grid(model ~ .)
+est_cox <- cox[, 
+               list(mean_sims=mean(exposure), 
+                    sd_sims=sqrt(var(exposure))), 
+               by=c("model")]
+
+# there are some very odd results from the weibull models... converging well
+# away from the solution. Drop them.
+out_restricted <- out[(log_scale > -100) |
+                      (param != "coef") | 
+                      (model == "coxfull") | 
+                      (model=="coxweighted")]
+weibull <- out_restricted[(model=="full" | model=="weighted"),] 
+h_weib <- ggplot(data=weibull[param=="coef"], aes(x=exposure))
+h_weib <- h_weib + geom_histogram() + facet_grid(model ~ .)
+est_weibull <- weibull[param=="coef",  
+                       list(mean=mean(exposure), sd=sqrt(var(exposure))),
+                       by=c("model")]
